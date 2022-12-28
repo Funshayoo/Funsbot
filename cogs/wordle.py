4892e1762d2e4290b925a1c4d655a7424fd7f4aa
Funshayoo <michalsmietana08@gmail.com>
2022-12-28 17:19:36 +0100

    started making wordle cog

---
+++ cogs/wordle.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Wordle(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.color = self.bot.embed_color
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Loaded wordle.py!')
+
+    # TODO wordle command
+    @app_commands.command(name="wordle", description="Play a game of wordle")
+    async def wordle(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Comming soon :eyes:",
+                              description="", color=self.color)
+        await interaction.response.send_message(embed=embed)
+
+
+async def setup(bot):
+    await bot.add_cog(Wordle(bot))
