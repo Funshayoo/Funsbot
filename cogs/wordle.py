495b11ca805f49860c598acff29ccf9de7f59341
Funshayoo <michalsmietana08@gmail.com>
2022-12-29 22:08:32 +0100

    added some fixed

--- cogs/wordle.py
+++ cogs/wordle.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import random
+from typing import List, Optional
 
 
 class Wordle(commands.Cog):
@@ -11,6 +12,11 @@ class Wordle(commands.Cog):
         self.is_playing = False
         self.answer = ""
         self.user_guess = ""
+        self.letters_colors = {
+            "green": "<:GreenSquare:1058105080630493244>",
+            "yellow": "<:YellowSquare:1058105081637122069>",
+            "grey": "<:ColorAbsent:1058105077073711144>"
+        }
 
         # * all word dictionaries
         self.popular = open(
@@ -23,7 +29,7 @@ class Wordle(commands.Cog):
         return random.choice(wordle_word)
 
     async def generate_blanks(self) -> str:
-        return ((":white_medium_square:" * 5) + ("\n")) * 6
+        return (("<:EmptySquare:1058105078709506168>" * 5) + ("\n")) * 6
 
     async def is_word_valid(self, word: str) -> bool:
         valid = False
@@ -35,21 +41,24 @@ class Wordle(commands.Cog):
             return valid
 
     async def generate_colored_word(self, guess: str, answer: str) -> str:
-        # colored_word = [EMOJI_CODES["gray"][letter] for letter in guess]
+
+        # colored_word = "0" * len(guess)
         # guess_letters: List[Optional[str]] = list(guess)
         # answer_letters: List[Optional[str]] = list(answer)
-        for i in range(len(guess)):
-            if guess[i] == answer[i]:
-                # colored_word[i] = EMOJI_CODES["green"][guess_letters[i]]
-                answer[i] = None
-                guess[i] = None
 
-        for i in range(len(guess)):
-            if guess[i] is not None and guess[i] in answer:
-                # colored_word[i] = EMOJI_CODES["yellow"][guess_letters[i]]
-                answer[answer.index(guess[i])] = None
+        # for i in range(len(guess)):
+        # for i in range(len(guess_letters)):
+        #     if guess_letters[i] == answer_letters[i]:
+        #         colored_word[i] = self.letters_colors["green"]
+        #         answer_letters[i] = None
+        #         guess_letters[i] = None
 
-        # return "".join(colored_word)
+        # for i in range(len(guess)):
+        #     if guess[i] is not None and guess[i] in answer:
+        #         colored_word[i] = self.letters_colors["yellow"]
+        #         answer[answer.index(guess[i])] = None
+
+        return "".join(answer)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -60,7 +69,7 @@ class Wordle(commands.Cog):
     async def play_wordle(self, interaction: discord.Interaction):
         if self.is_playing:
             embed = discord.Embed(
-                title="", description="Game is already started", color=self.color)
+                title="", description="Game has already started", color=self.color)
             await interaction.response.send_message(
                 embed=embed, ephemeral=True)
         else:
@@ -71,6 +80,7 @@ class Wordle(commands.Cog):
             embed = discord.Embed(title="Guees the word using: **/wordle_guess**",
                                   description=f"{blanks}", color=self.color)
             await interaction.response.send_message(embed=embed)
+            print(self.answer)
 
     @ app_commands.command(name="wordle_guess", description="Guess a word")
     @ app_commands.describe(guess="Your guess")
@@ -87,12 +97,13 @@ class Wordle(commands.Cog):
                 await interaction.channel.send(f"{guess}")
                 await interaction.channel.purge(limit=2)
                 color_word = await self.generate_colored_word(self.user_guess, self.answer)
+                await interaction.channel.send(f"{color_word}")
             else:
                 embed = discord.Embed(
-                    title="", description="Your word is not in dictionary", color=self.color)
+                    title="", description="Your word is not in the dictionary", color=self.color)
                 await interaction.response.send_message(embed=embed)
 
-    @ app_commands.command(name="wordle_stats", description="view wordle stats")
+    @ app_commands.command(name="wordle_stats", description="view your wordle stats")
     async def wordle_stats(self, interaction: discord.Interaction):
         pass
 
