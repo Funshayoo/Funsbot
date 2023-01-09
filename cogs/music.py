cfc985fd30b26752974846bb60db9f63aeb6089f
Michal Smietana <michalsmietana08@gmail.com>
2023-01-09 20:12:52 +0000

    fixed audio speed at start

--- cogs/music.py
+++ cogs/music.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from youtube_dl import YoutubeDL
+import asyncio
 
 # TODO add some buttons
 
@@ -47,6 +48,9 @@ class Music(commands.Cog):
 
             self.vc.play(discord.FFmpegPCMAudio(
                 m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+            self.vc.pause()
+            await asyncio.sleep(1)
+            self.vc.resume()
         else:
             self.is_playing = False
 
@@ -76,6 +80,9 @@ class Music(commands.Cog):
 
             self.vc.play(discord.FFmpegPCMAudio(
                 m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+            self.vc.pause()
+            await asyncio.sleep(1)
+            self.vc.resume()
         else:
             self.is_playing = False
 
