9c33b109efa85fdc0c1262ae8542964002ca3888
Funshayoo <michalsmietana08@gmail.com>
2023-01-31 11:57:00 +0100

    fixed database

--- cogs/database.py
+++ cogs/database.py
@@ -19,23 +19,27 @@ class Database(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('Loaded database.py!')
+
         async with aiosqlite.connect(database_dir) as wordle_db:
             async with wordle_db.cursor() as wordle_cursor:
-                await wordle_cursor.execute("CREATE TABLE IF NOT EXISTS main (user_id INTEGER, guild_id INTEGER)")
+                await wordle_cursor.execute("CREATE TABLE IF NOT EXISTS main (user_id INTEGER, todays_word STRING, can_guess BOOLEAN, games INTEGER, wins INTEGER, losses INTEGER)")
             await wordle_db.commit()
 
-    async def test_database(self, interaction: discord.Interaction):
+    @app_commands.command(name="add_user", description="add user to database")
+    async def add_user(self, interaction: discord.Interaction):
         user = interaction.user
-        guild = interaction.guild
+        word = "apple"
+
         async with aiosqlite.connect(database_dir) as wordle_db:
             async with wordle_db.cursor() as wordle_cursor:
-                await wordle_cursor.execute("SELECT user_id FROM main WHERE guild_id = ?", (guild.id,))
+                await wordle_cursor.execute(f"SELECT user_id FROM main WHERE user_id = {user.id}")
                 data = await wordle_cursor.fetchone()
-                if data:
-                    await wordle_cursor.execute("UPDATE main SET id = ? WHERE guild_id = ?", (user.id, guild.id))
-                else:
-                    await wordle_cursor.execute("INSERT INTO main (user_id, guild_id) VALUES (?, ?)", (user.id, guild.id))
+                print(data)
+                if data is None:
+                    await wordle_cursor.execute(f"INSERT INTO main (user_id, todays_word, can_guess, games, wins, losses) VALUES {user.id, word, True, 0, 0, 0}")
             await wordle_db.commit()
+
+        await self.bot.embed(interaction, "", title="Added user to the database")
 
 
 async def setup(bot):
