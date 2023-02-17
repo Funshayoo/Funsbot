dbaf871c9d4c495479c6e7af85010cc4d5bc24c5
Funshayo_o <michalsmietana08@gmail.com>
2023-02-17 14:41:30 +0100

    added tries footer

--- cogs/wordle.py
+++ cogs/wordle.py
@@ -157,7 +157,7 @@ class Wordle(commands.Cog):
             else:
                 await self.remove_try(interaction)
                 colored_word = self.generate_colored_word(guess, self.answer)
-                await self.bot.embed(interaction, f"{colored_word} {guess}", "Your guess:")
+                await self.bot.embed(interaction, f"{colored_word} {guess}", title="Your guess:", footer=f"tries left: {self.tries_left}")
 
     @ app_commands.command(name="wordle_stats", description="View your wordle stats")
     async def wordle_stats(self, interaction: discord.Interaction):
