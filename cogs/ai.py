b23073f4e86897fe3e56aef5485cf33f44c0873d
Funshayoo <michalsmietana08@gmail.com>
2023-04-08 18:46:54 +0200

    modified:   cogs/ai.py

--- cogs/ai.py
+++ cogs/ai.py
@@ -18,14 +18,20 @@ class Ai(commands.Cog):
     @app_commands.command(name="ask_gpt", description="Ask GPT-3 a question")
     @app_commands.describe(prompt="Enter your prompt")
     async def ask_gpt(self, interaction: discord.Interaction, prompt: str):
+        await interaction.response.defer()
+
         openai.api_key = Config.OPENAI_API_KEY
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
             max_tokens=64,
+            temperature=0.9,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6,
         )
 
-        await self.bot.embed(interaction, title="ChatGPT response: ", description=response.choices[0].text)
+        await self.bot.embed(interaction, title="ChatGPT response: ", description=response.choices[0].text, followup=True)
 
 
 async def setup(bot):
