198333319c9ccce85a643517376a5cd54996696c
Funshayoo <michalsmietana@gmail.com>
2023-04-08 15:08:30 +0200

    added ai.py

---
+++ cogs/ai.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from config import Config
+
+import openai
+
+
+class Ai(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Loaded ai.py!')
+
+    @app_commands.command(name="ask_gpt", description="Ask GPT-3 a question")
+    @app_commands.describe(prompt="Enter your prompt")
+    async def ask_gpt(self, interaction: discord.Interaction, prompt: str):
+        openai.api_key = Config.OPENAI_API_KEY
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=64,
+        )
+
+        await self.bot.embed(interaction, title="ChatGPT response: ", description=response.choices[0].text)
+
+
+async def setup(bot):
+    await bot.add_cog(Ai(bot))
