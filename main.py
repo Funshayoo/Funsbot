1b2356620b428d4ec8b935e699ec3203ff7ee91d
funshayo <fanseyo@protonmail.com>
2023-09-10 08:57:47 +0200

    created utils/ directory and removed useless code from database.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 import asyncio
 from config import Config
+from cogs.utils.database import Database
 
 import os
 
@@ -13,7 +14,7 @@ class Funsbot(commands.AutoShardedBot):
         super().__init__(*args, **kwargs)
         self.embed_color = 0x2F3136
 
-    # Important embed function
+    # Frequently used embed function
     async def embed(self, interaction: discord.Interaction, description: str, title: str = None, ephemeral: bool = False, view: discord.ui.View = None, footer: str = None, followup: bool = False):
         embed = discord.Embed(
             description=description, color=self.embed_color)
@@ -32,10 +33,6 @@ intents = discord.Intents().all()
 bot = Funsbot(command_prefix="/", intents=intents, help_command=None)
 tree = bot.tree
 
-color = bot.embed_color
-
-# load all cogs
-
 
 async def load_extensions():
     for filename in os.listdir('./cogs'):
@@ -49,7 +46,7 @@ async def on_ready():
     # change bot activity
     await bot.change_presence(activity=discord.Game(name='przezycie z pania z edb'))
     print(f"""
-          _   _       _                       
+  _   _       _                       
  | \ | |     (_)                      
  |  \| | __ _ _ _ __ ___   __ _ _ __  
  | . ` |/ _` | | '_ ` _ \ / _` | '_ \ 
@@ -64,14 +61,16 @@ async def on_ready():
     # ! sync all commands
     try:
         await tree.sync()
-    except Exception as e:
-        print(e)
+    except Exception as error:
+        print(error)
 
 # start the bot
 
 
 async def main():
     async with bot:
+        # create a database at start
+        await Database().db_create()
         await load_extensions()
         await bot.start(Config.DISCORD_TOKEN)
 asyncio.run(main())
